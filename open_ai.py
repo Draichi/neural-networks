cf5a38f16bbb3195489e49f75cce74a1ebe32402
Draichi <lucasdraichi@gmail.com>
2018-02-05 19:48:15 -0200

    training data

--- open_ai.py
+++ open_ai.py
@@ -15,6 +15,7 @@ goal_steps = 500
 score_requirement = 50
 inital_games = 10000
 
+# random game
 def some_random_games_first():
     for episode in range(5):
         env.reset()
@@ -24,4 +25,41 @@ def some_random_games_first():
             observation, reward, done, info = env.step(action)
             if done:
                 break
-some_random_games_first()
+#some_random_games_first()
+
+def initial_population():
+    training_data = []
+    scores = []
+    accepted_scores = []
+    for _ in range(inital_games):
+        score = 0
+        game_memory = []
+        prev_observation = []
+        for _ in range(goal_steps):
+            # oly rearange 0's and 1's
+            action = random.randrange(0,2)
+            observation, reward, done, info = env.step(action)
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation, action])
+            prev_observation = observation
+            # reward = 0, 1
+            score += reward
+            if done:
+                break
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1,0]
+                training_data.append([data[0], output])
+        env.reset()
+        scores.append(score)
+    training_data_save = np.array(training_data)
+    np.save('saved.npy', training_data_save)
+    print('Average accepted score:', mean(accepted_scores))
+    print('Median accepted score:', median(accepted_scores))
+    print(Counter(accepted_scores))
+    return training_data
+initial_population()
